cb8ab982cda1ec45fc25d58ac119404899c85594
Enrickyb <enrickyb@hotmail.com>
2024-10-22 20:46:24 -0400

    chore: adding lottie example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1 className="text-black">Hello World</h1>
+      <DotLottieReact src="/src/assets/lotties/0H93rE5C00.json" loop autoplay />
+      <DotLottieReact src="/src/assets/lotties/oDS3JIG0kc.json" loop autoplay />
     </>
   );
 }
