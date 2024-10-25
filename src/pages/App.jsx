b732d8a5312214922ccd84873f420929ceb6f3e1
Enrickyb <enrickyb@hotmail.com>
2024-10-25 15:21:34 -0400

    feat: adding schedule section

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,9 @@
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import Schedule from "../components/Schedule";
 
 function App() {
   return (
     <>
-      <h1 className="text-black font-poppins">Hello World</h1>
-      <DotLottieReact src="/src/assets/lotties/0H93rE5C00.json" loop autoplay />
-      <DotLottieReact src="/src/assets/lotties/oDS3JIG0kc.json" loop autoplay />
+      <Schedule />
     </>
   );
 }
