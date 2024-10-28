e745fc19b980ff9f50eaf04b371517ead9b87abd
Enrickyb <enrickyb@hotmail.com>
2024-10-28 07:45:55 -0400

    adding Schedule to home page

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,14 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Submissions from "../components/Submissions";
+import Schedule from "../components/Schedule";
 function App() {
   return (
     <>
       <Header />
       <Hero />
       <Submissions />
+      <Schedule />
     </>
   );
 }
