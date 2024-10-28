e4d5b309b755198710e0aba8a6e058dc4f894a0a
alanBMC <alanbmc00@gmail.com>
2024-10-27 20:52:36 -0400

    feat: adicionando assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
-
+import SIEGEPAN from "./componentes/HEROPAGE2";
 function App() {
   return (
     <>
       <h1 className="text-black">Hello World</h1>
+      <div>
+      <SIEGEPAN />
+      </div>
     </>
   );
 }
