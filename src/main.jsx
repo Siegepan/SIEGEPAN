d7aa878cb0f1aa5b3d416c94f428ebabc240fbf3
Enrickyb <enrickyb@hotmail.com>
2024-10-28 07:38:59 -0400

    resolving merge conflits

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./pages/App";
 import "./index.css";
-import Hero from "./components/Hero/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
