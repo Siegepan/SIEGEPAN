05fd161842e53af951ff8d1b474d3e08394a87ab
AlissonJLO <alissonloaquim@hotmail.com>
2024-10-27 23:25:23 -0400

    fix: retirando o header do main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Header from './components/Header'
+import App from './pages/App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header></Header>
+    <App />
   </StrictMode>,
 )
