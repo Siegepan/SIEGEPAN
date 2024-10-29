5fd134c2051f9a704324323982b79baa67fac1b5
AlissonJLO <alissonloaquim@hotmail.com>
2024-10-28 23:33:30 -0400

    fix: colocando o app de volta no root

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Hero from './components/Hero/index.jsx'
+import App from './pages/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Hero />
+    <App />
   </StrictMode>,
 )
