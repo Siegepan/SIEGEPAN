717dde415c0d6b4f6a73cd4d265d670509987385
Enrickyb <enrickyb@hotmail.com>
2024-10-30 10:22:32 -0400

    adding react swiper lib

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ import Submissions from "./components/Submissions";
 import Schedule from "./components/Schedule";
 
 import Header from "./components/Header";
+import { Convidados } from "./components/Convidados";
 function App() {
   return (
     <>
       <div>
-        <Header />
-        <Hero />
-        <Submissions />
-        <Schedule />
-        <Participar />
+        <Convidados />
       </div>
     </>
   );
---
+++ src/components/Convidados/index.jsx
@@ -0,0 +1,11 @@
+import Swiper from "./Swiper";
+
+const Convidados = () => {
+  return (
+    <div className="">
+      <Swiper />
+    </div>
+  );
+};
+
+export default Convidados;
