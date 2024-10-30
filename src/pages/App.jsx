8cd5468d0b4c87546064c683216e34b966e62e27
Enrickyb <enrickyb@hotmail.com>
2024-10-30 08:36:38 -0400

    fix responsive content

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Submissions from "../components/Submissions";
 import Schedule from "../components/Schedule";
+import SobreSiegepan from "../components/Sobre";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Submissions />
       <Schedule />
+      <SobreSiegepan />
     </>
   );
 }
