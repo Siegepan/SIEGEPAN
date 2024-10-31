0ac5c867513dc7c6c16469c3aa684d5595806859
Enrickyb <enrickyb@hotmail.com>
2024-10-31 09:35:58 -0400

    Revert "Merge branch 'parceiros' into develop"
    This reverts commit 04ea79f35b6cb677acfa4b8cc9e79e7d191d165e, reversing
    changes made to 89cb765f410aaf366db953422e0ea38228d69637.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -6,7 +6,6 @@ import SobreSiegepan from "../components/Sobre";
 import FAQAccordion from "../components/Faq";
 import Footer from "../components/Footer";
 import FiquePorDentro from "../components/FiquePorDentro";
-import Parceiros from "../components/Parceiros";
 function App() {
   return (
     <>
@@ -18,7 +17,6 @@ function App() {
       <SobreSiegepan />
       <FAQAccordion />
       <Footer />
-      <Parceiros />
     </>
   );
 }
